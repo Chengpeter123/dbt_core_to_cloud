9dc59d992cba3c5b169ce19b4742cb65a448ed21
Chengpeter123 <chengpeter940@gmail.com>
2023-11-09 14:18:04 +0530

    Initial commit

---
+++ models/Staging/STG_LEADS.sql
@@ -0,0 +1,10 @@
+{{ 
+        config(
+        materialized="table",
+        enabled=true,
+        database= 'DBT_INFOFISCUS_STAGING_DEV'
+    
+        ) 
+}}
+
+    select * from {{ref('VW_LEADS')}}
